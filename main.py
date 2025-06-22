ab526e01d903fe8582502f1d44bcc1184d386491
hoseinebrahimi1 <mhoseinebrahimi80@gmail.com>
2025-06-22 22:11:00 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
 import subprocess
 import sys
 import os
-API_KEY = os.getenv("MY_API_KEY")
+
+# گرفتن مقدار کلید از محیط (تعریف‌شده در Render)
+API_KEY = os.getenv("MY-API-KEY")  # چون تو Render این کلیدو به اسم MY-API-KEY ذخیره کردی
 
 app = FastAPI()
+
+# تابع اعتبارسنجی
+def verify_api_key(request: Request):
+    client_key = request.headers.get("X-API-Key")
+    if client_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Invalid API Key")
 
 @app.get("/ping")
 async def ping():
     return {"pong": "ok"}
 
 @app.post("/run/roja")
-async def run_roja():
+async def run_roja(request: Request):
+    verify_api_key(request)
     p = subprocess.run(
         [sys.executable, "roja_shop_data.py"],
         capture_output=True,
@@ -24,7 +33,8 @@ async def run_roja():
     }
 
 @app.post("/run/khanoumi")
-async def run_khanoumi():
+async def run_khanoumi(request: Request):
+    verify_api_key(request)
     p = subprocess.run(
         [sys.executable, "khanoumi_data.py"],
         capture_output=True,
